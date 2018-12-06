1ec9784d2483ac225a82977659196c595cefba5e
Alexander Klus <Alexander_@hotmail.nl>
2018-12-05 23:59:11 -0800

    Add files via upload

---
+++ precache-manifest.64608429831af2bc2da62ee4cdce3142.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/iconsold/apple-touch-icon.png"
+  },
+  {
+    "revision": "64373286793e3c8b2b4e3219cbf3544b",
+    "url": "/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/iconsold/favicon-16x16.png"
+  },
+  {
+    "revision": "39dc89fe4a9030739c8d",
+    "url": "/js/app.191e5323.js"
+  },
+  {
+    "revision": "ac62181563633c493a690b61bb668740",
+    "url": "/saved_model_transformer_js/group1-shard2of2"
+  },
+  {
+    "revision": "ece96f82d003bf8ff6156feaa0166f0d",
+    "url": "/saved_model_transformer_js/group1-shard1of2"
+  },
+  {
+    "revision": "556309962e454e5472f29f4686a94434",
+    "url": "/saved_model_transformer_js/tensorflowjs_model.pb"
+  },
+  {
+    "revision": "0fc3424e67c513d860801b24d5b12a65",
+    "url": "/saved_model_style_js/tensorflowjs_model.pb"
+  },
+  {
+    "revision": "d19e4407dbada619d70340a780e4adab",
+    "url": "/saved_model_style_js/group1-shard1of3"
+  },
+  {
+    "revision": "b54c8de769326f16d4d6fad1b699f54d",
+    "url": "/saved_model_style_js/group1-shard3of3"
+  },
+  {
+    "revision": "8eba84bf2ab278b680ba890281acb5f3",
+    "url": "/saved_model_style_js/group1-shard2of3"
+  },
+  {
+    "revision": "b7692e88ab0ecd2388ef4226bbfc8e4d",
+    "url": "/saved_model_style_inception_js/group1-shard8of9"
+  },
+  {
+    "revision": "d022b62f3079345f90f0e2189434a2fe",
+    "url": "/saved_model_style_inception_js/group1-shard6of9"
+  },
+  {
+    "revision": "412f52ee5397906dd62d7a4ff92e0d56",
+    "url": "/saved_model_style_inception_js/group1-shard9of9"
+  },
+  {
+    "revision": "fb590e25e290dbe043a6982ecf0a3945",
+    "url": "/saved_model_style_inception_js/group1-shard7of9"
+  },
+  {
+    "revision": "f057e9b9a98a6db48dc09de11721cda2",
+    "url": "/saved_model_style_inception_js/tensorflowjs_model.pb"
+  },
+  {
+    "revision": "e437786d4ec410ead22d18bad8cc6dd0",
+    "url": "/saved_model_style_inception_js/group1-shard5of9"
+  },
+  {
+    "revision": "cf98a53d98288a92e6068a185d44e645",
+    "url": "/saved_model_style_inception_js/group1-shard1of9"
+  },
+  {
+    "revision": "2808b2cb9166526c187e753b2f203970",
+    "url": "/saved_model_style_inception_js/group1-shard2of9"
+  },
+  {
+    "revision": "ae673b54b7831c3f627bac114a52ff92",
+    "url": "/saved_model_style_inception_js/group1-shard4of9"
+  },
+  {
+    "revision": "d715426a83b067670e866cf87250960a",
+    "url": "/saved_model_style_inception_js/group1-shard3of9"
+  },
+  {
+    "revision": "504a07e7fbfa18561082ffb7efd13362",
+    "url": "/python.py"
+  },
+  {
+    "revision": "5648da97772720265407d568d9487067",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "559cc428cd9609440d5a20611fe24479",
+    "url": "/manifestold.json"
+  },
+  {
+    "revision": "0d9ea28e4526795a67be",
+    "url": "/js/chunk-vendors.0b9f7781.js"
+  },
+  {
+    "revision": "a35587cfdc24e867e3c1dd270d96ac17",
+    "url": "/images/udnie.jpg"
+  },
+  {
+    "revision": "78a132c1abc2e5719e47",
+    "url": "/js/about.826b3e79.js"
+  },
+  {
+    "revision": "2443f87f95564e401f3eab938da84a49",
+    "url": "/images/towers.jpg"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/iconsold/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/iconsold/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/iconsold/mstile-150x150.png"
+  },
+  {
+    "revision": "04607013acd8a89ea8e82922ff9c01f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/iconsold/favicon-32x32.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/img/iconsold/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "99850e33f60db644dd1fbaeae44d407f",
+    "url": "/images/seaport.jpg"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/iconsold/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "60cd4da86a7ff1fd76e8e6f7cf968d97",
+    "url": "/images/red_circles.jpg"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/iconsold/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/iconsold/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/iconsold/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "531565534b6f3d690f2be43ce51d83c2",
+    "url": "/images/golden_gate.jpg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/iconsold/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/iconsold/android-chrome-512x512.png"
+  },
+  {
+    "revision": "8262bbf4546363eee19ee4f9c5cbcf14",
+    "url": "/images/chicago.jpg"
+  },
+  {
+    "revision": "b13b6dee513b7fb49ff620bd2e86f211",
+    "url": "/images/statue_of_liberty.jpg"
+  },
+  {
+    "revision": "63af32f948f09d7c70ef6d9305534c05",
+    "url": "/images/zigzag.jpg"
+  },
+  {
+    "revision": "f6c02290f4dce29b8d29a50132fb3c28",
+    "url": "/images/stripes.jpg"
+  },
+  {
+    "revision": "81a86fa5e8adca9a8ad1f4d6d449f1d2",
+    "url": "/images/stata.jpg"
+  },
+  {
+    "revision": "e8775eee466b9866e2851f96bcec1ce4",
+    "url": "/images/sketch.jpg"
+  },
+  {
+    "revision": "916912125855b83acdc3ccbf04ab9d14",
+    "url": "/images/diana.jpg"
+  },
+  {
+    "revision": "6539997a656a4f224b77f0d812b71b82",
+    "url": "/images/clouds.jpg"
+  },
+  {
+    "revision": "a13dacc5ef30092ce9872c3471cc26c7",
+    "url": "/images/bricks.jpg"
+  },
+  {
+    "revision": "c21c5f04f96c8a06dfb3092a96fb3f4b",
+    "url": "/images/beach.jpg"
+  },
+  {
+    "revision": "39dc89fe4a9030739c8d",
+    "url": "/css/app.a91b337f.css"
+  }
+];
